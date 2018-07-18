de169696a16e3e4376d904041b3f9246f73695f0
来者可是诸葛bloo <15152278073@139.com>
2018-07-18 12:12:14 +0800

    [code] adjust for master

--- component_magnet/src/main/java/me/jbusdriver/component/magnet/ui/fragment/MagnetListFragment.kt
+++ component_magnet/src/main/java/me/jbusdriver/component/magnet/ui/fragment/MagnetListFragment.kt
@@ -16,12 +16,12 @@ import me.jbusdriver.base.common.C
 import me.jbusdriver.base.mvp.bean.Magnet
 import me.jbusdriver.base.mvp.ui.adapter.BaseAppAdapter
 import me.jbusdriver.component.magnet.R
+import me.jbusdriver.component.magnet.loader.MagnetFormatPrefix
 import me.jbusdriver.component.magnet.mvp.Contract.MagnetListContract
-import me.jbusdriver.component.magnet.mvp.Contract.MagnetListContract.MagnetListPresenter
 import me.jbusdriver.component.magnet.mvp.presenter.MagnetListPresenterImpl
 import org.jsoup.Jsoup
 
-class MagnetListFragment : AppBaseRecycleFragment<MagnetListPresenter, MagnetListContract.MagnetListView, Magnet>(), MagnetListContract.MagnetListView {
+class MagnetListFragment : AppBaseRecycleFragment<MagnetListContract.MagnetListPresenter, MagnetListContract.MagnetListView, Magnet>(), MagnetListContract.MagnetListView {
 
     private val keyword by lazy { arguments?.getString(C.BundleKey.Key_1) ?: error("need keyword") }
     private val magnetLoaderKey by lazy {
@@ -52,23 +52,30 @@ class MagnetListFragment : AppBaseRecycleFragment<MagnetListPresenter, MagnetLis
             }
 
         }.apply {
+
+            fun tryGetMagnet(url: String): Flowable<String> {
+                return Flowable.just(url).flatMap { url ->
+                    if (url.startsWith(MagnetFormatPrefix)) {
+                        Flowable.just(url)
+                    } else {
+                        Flowable.fromCallable { Jsoup.connect(url).get().select(".content .magnet").text().trim() }
+                                .addUserCase(sec = 6)
+                                .onErrorReturn { url }
+                    }
+                }
+            }
+
             setOnItemClickListener { adapter, _, position ->
                 (adapter.data.getOrNull(position) as? Magnet)?.let { magnet ->
                     KLog.d("setOnItemClickListener $magnet")
                     showMagnetLoading()
-                    Flowable.just(magnet.link).flatMap { url ->
-                        if (url.startsWith("magnet:?xt")) {
-                            Flowable.just(url)
-                        } else {
-                            Flowable.fromCallable { Jsoup.connect(url).get().select(".content .magnet").text().trim() }
-                                    .addUserCase(sec = 6)
-                                    .onErrorReturn { url }
-                        }
-                    }.compose(SchedulersCompat.io()).subscribeBy {
-                        viewContext.browse(it) {
-                            placeDialogHolder?.dismiss()
-                        }
-                    }.addTo(rxManager)
+                    tryGetMagnet(magnet.link)
+                            .compose(SchedulersCompat.io()).subscribeBy {
+                                this@MagnetListFragment.adapter.setData(position, magnet.copy(link = it))
+                                viewContext.browse(it) {
+                                    placeDialogHolder?.dismiss()
+                                }
+                            }.addTo(rxManager)
 
                 }
 
@@ -78,11 +85,16 @@ class MagnetListFragment : AppBaseRecycleFragment<MagnetListPresenter, MagnetLis
                 (adapter.data.getOrNull(position) as? Magnet)?.let { magnet ->
                     when (view.id) {
                         R.id.iv_magnet_copy -> {
-                            KLog.d("copy $magnet")
-                            view.context.apply {
-                                copy(magnet.link)
-                                toast("复制成功")
-                            }
+
+                            tryGetMagnet(magnet.link).compose(SchedulersCompat.io()).subscribeBy { url->
+                                this@MagnetListFragment.adapter.setData(position, magnet.copy(link = url))
+                                view.context.apply {
+                                    copy(url)
+                                    toast("复制成功")
+                                }
+                                KLog.d("copy value : ${view.context.paste()}")
+                            }.addTo(rxManager)
+
 
                         }
                         else -> Unit
